17f58b87adedec4ffddf2a2ee53c4cd9fff8465a
Creativecoder11 <creativecoderpro@gmail.com>
2023-03-26 00:45:06 +0600

    Home Banner Fixed

--- src/Components/HomeBanner.js
+++ src/Components/HomeBanner.js
@@ -12,9 +12,9 @@ const HomeBanner = () => {
   });
 
   return (
-    <section className="md:mx-20 text-white h-[80vh] flex items-center gap-8">
+    <section className="md:mx-20 text-white h-[80vh] flex items-center justify-center gap-8">
       <div className="text-center md:text-start md:w-1/2">
-        <h2 className="text-5xl md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-sky-500">
+        <h2 className="text-5xl md:text-6xl lg:text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-sky-500">
           {data?.BannerTitle}
         </h2>
         <p className="text-lg md:text-2xl text-gray-400 mt-5">
